665a3465d1938c14eae9455c44bb4c108132e3d9
Hour477 <brohour00044@gmail.com>
2026-06-29 23:12:48 +0700

    fix owner dashboard status filter query mapping

--- scratch_order.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$order = App\Models\Order::with(['items.productVariant.product'])->first();
-if ($order) {
-    echo json_encode($order->toArray(), JSON_PRETTY_PRINT);
-}
--- scratch_test_svg_inspect.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-$generator = new SimpleSoftwareIO\QrCode\Generator;
-$svg = $generator->format('svg')->size(100)->generate('test');
-echo $svg;
