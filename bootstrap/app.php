3896d952802795061b9d545743a45abe919039fb
Hour477 <brohour00044@gmail.com>
2026-07-01 01:55:52 +0700

    fix: trust proxies and force HTTPS scheme in production to resolve mixed content error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->prepend(\App\Http\Middleware\CorsMiddleware::class);
         $middleware->api(append: [
             \App\Http\Middleware\DecodeHashids::class,
